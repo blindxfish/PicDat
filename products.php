cbaebf4b91fc3836ea68c4ff1b5890bb209bc4e1
Nixon <blindxfish@gmail.com>
2021-01-07 12:19:46 +0100

    comment

--- products.php
+++ products.php
@@ -1,4 +1,3 @@
-
 <?php 
 
 function getStringBetween($str,$from,$to)
@@ -8,65 +7,59 @@ function getStringBetween($str,$from,$to)
 }
 
 $dir_name = "./datages/";
-$images = glob($dir_name."*.*");
+$images = glob($dir_name."*.*"); ?>
 
-?>
-
-<div class="container-fluid">
-<div style="height:100px">
-    </div>
-    
-    <div class="row justify-content-md-center">
-        <div class="container">
-            <div class="col md-auto">
-                    <ul class="nav nav-pills nav-fill">
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Go to Upload</a>
-                </li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <div style="height:100px">
         </div>
-    </div>
-    <div style="height:10px">
-    </div>
-    
-    <div class="row justify-content-md-center">
-    <div class="container">
-        <div class="col md-auto">
-        
-            <div class="panel-group">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Listing</div>
-                    <div class="panel-body">
-                    <div class="card-columns">
-                    <?php
-                    foreach($images as $image) { 
-
-                        $price = getStringBetween($image, "---p-", "---l-");
-                        $productname =  getStringBetween($image, "S-p-n-", "---t-");   //productname from S-p-n-   to  ---t-
-                        $description =  getStringBetween($image, "---d-", "---p-"); 
-                        $user = getStringBetween($image, "---u-", "."); 
-                        $date = getStringBetween($image, "---t-", "---n-"); 
-                        ?>
-                        <div class="card">
-                        <img class="card-img-top" src="<?php echo $image; ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title"><?php echo $productname." for - $".$price; ?></h4>
-                            <p class="card-text"><?php echo $description; ?></p>  
-                            <p class="card-text"><small class="text-muted">uploaded by <?php echo $user." on date ".$date; ?></small></p>
-                        </div>
-                        </div>
-                        
-                        <?php
-                        //var_dump($image);
-                        //echo '<img src="'.$image.'" /><br />';
-                    }
-                    
-                 ?></div>
+        <div class="row justify-content-md-center">
+            <div class="container">
+                <div class="col md-auto">
+                        <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Go to Upload</a>
+                    </li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </div>
-    </div></div>
-    
-    
+        <div style="height:10px">
+        </div>
+        <div class="row justify-content-md-center">
+        <div class="container">
+            <div class="col md-auto">
+                <div class="panel-group">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Listing</div>
+                            <div class="panel-body">
+                                <div class="card-columns">
+                                    <?php
+                                    foreach($images as $image) { 
+
+                                        $price = getStringBetween($image, "---p-", "---l-");
+                                        $productname =  getStringBetween($image, "S-p-n-", "---t-");   //productname from S-p-n-   to  ---t-
+                                        $description =  getStringBetween($image, "---d-", "---p-"); 
+                                        $user = getStringBetween($image, "---u-", "."); 
+                                        $date = getStringBetween($image, "---t-", "---n-"); 
+                                        ?>
+                                            <div class="card">
+                                                <img class="card-img-top" src="<?php echo $image; ?>" alt="Card image cap">
+                                                <div class="card-body">
+                                                    <h4 class="card-title"><?php echo $productname." for - $".$price; ?></h4>
+                                                    <p class="card-text"><?php echo $description; ?></p>  
+                                                    <p class="card-text"><small class="text-muted">uploaded by <?php echo $user." on date ".$date; ?></small></p>
+                                                </div>
+                                            </div>
+                                        
+                                        <?php
+                                        //var_dump($image);
+                                        //echo '<img src="'.$image.'" /><br />';
+                                    } ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>   
+    </div> 
--- upload.php
+++ upload.php
@@ -1,8 +1,7 @@
-
+<?php //This file contains the upload form ?>
 <div class="container-fluid">
-<div style="height:100px">
+    <div style="height:100px">
     </div>
-    
     <div class="row justify-content-md-center">
         <div class="container">
             <div class="col md-auto">
@@ -17,41 +16,42 @@
     <div style="height:10px">
     </div>
     <div class="row justify-content-md-center">
-    <div class="container">
-        <div class="col md-auto">
-        
-            <div class="panel-group">
-                <div class="panel panel-default">
-                    <div class="panel-heading">PicDat v1.0</div>
-                    <div class="panel-body">
+        <div class="container">
+            <div class="col md-auto">
+            
+                <div class="panel-group">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">PicDat v1.0</div>
+                        <div class="panel-body">
 
-                        <form id="daform" action="action.php" method="POST" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label for="customFile">Select picture:</label>
-                                
-                                <!-- Upload file  -->
-                                <input type="file" class="form-control" id="customFile" name="img" accept="image/*" />
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label" for="productname">Product name ( max 20characters )</label>
-                                <input type="text" id="productname" name="productname" class="form-control" placeholder="Batman Action Figure">
+                            <form id="daform" action="action.php" method="POST" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="customFile">Select picture:</label>
+                                    
+                                    <!-- Upload file  -->
+                                    <input type="file" class="form-control" id="customFile" name="img" accept="image/*" />
                                 </div>
-                            <div class="form-group">   
-                                <label for="price">Price</label>
-                                <input type="number" id="price" name="price" min="1" max="1000000">
+                                <div class="form-group">
+                                    <label class="form-label" for="productname">Product name ( max 20characters )</label>
+                                    <input type="text" id="productname" name="productname" class="form-control" placeholder="Batman Action Figure">
+                                    </div>
+                                <div class="form-group">   
+                                    <label for="price">Price</label>
+                                    <input type="number" id="price" name="price" min="1" max="1000000">
+                                    </div>
+                                <div class="form-group">                                   
+                                    <label for="desc">Description ( max 150 characters ) </label>
+                                    <textarea class="form-control"  name="desc" rows="5" id="desc"></textarea>
+                                    </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
-                            <div class="form-group">                                   
-                                <label for="desc">Description ( max 150 characters ) </label>
-                                <textarea class="form-control"  name="desc" rows="5" id="desc"></textarea>
-                                </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
+                            </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    </div></div>
+</div>
     
     
